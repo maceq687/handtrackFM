a48220ad3b2603f2d18b08059f16ee9c202a3a1b
maceq687 <maciek@softman.pl>
2023-01-04 22:23:10 +0100

    replaced template files

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let device;
+
 async function setup() {
     const patchExportURL = "export/patch.export.json";
 
@@ -41,18 +43,8 @@ async function setup() {
         return;
     }
     
-    // (Optional) Fetch the dependencies
-    let dependencies = [];
-    try {
-        const dependenciesResponse = await fetch("export/dependencies.json");
-        dependencies = await dependenciesResponse.json();
-
-        // Prepend "export" to any file dependenciies
-        dependencies = dependencies.map(d => d.file ? Object.assign({}, d, { file: "export/" + d.file }) : d);
-    } catch (e) {}
 
     // Create the device
-    let device;
     try {
         device = await RNBO.createDevice({ context, patcher });
     } catch (err) {
@@ -64,9 +56,6 @@ async function setup() {
         return;
     }
 
-    // (Optional) Load the samples
-    if (dependencies.length)
-        await device.loadDataBufferDependencies(dependencies);
 
     // Connect the device to the web audio graph
     device.node.connect(outputNode);
@@ -76,18 +65,6 @@ async function setup() {
 
     // (Optional) Automatically create sliders for the device parameters
     makeSliders(device);
-
-    // (Optional) Create a form to send messages to RNBO inputs
-    makeInportForm(device);
-
-    // (Optional) Attach listeners to outports so you can log messages from the RNBO patcher
-    attachOutports(device);
-
-    // (Optional) Load presets, if any
-    loadPresets(device, patcher);
-
-    // (Optional) Connect MIDI inputs
-    makeMIDIKeyboard(device);
 
     document.body.onclick = () => {
         context.resume();
@@ -208,129 +185,79 @@ function makeSliders(device) {
     });
 }
 
-function makeInportForm(device) {
-    const idiv = document.getElementById("rnbo-inports");
-    const inportSelect = document.getElementById("inport-select");
-    const inportText = document.getElementById("inport-text");
-    const inportForm = document.getElementById("inport-form");
-    let inportTag = null;
-    
-    // Device messages correspond to inlets/outlets or inports/outports
-    // You can filter for one or the other using the "type" of the message
-    const messages = device.messages;
-    const inports = messages.filter(message => message.type === RNBO.MessagePortType.Inport);
+// Computer vision
+navigator.getUserMedia = 
+    navigator.getUserMedia 
+    || navigator.webkitgetUserMedia
+    || navigator.mozGetgetUserMedia 
+    || navigator.msGetgetUserMedia;
 
-    if (inports.length === 0) {
-        idiv.removeChild(document.getElementById("inport-form"));
-        return;
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 1,         // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.7,    // confidence threshold for predictions.
+}
+
+const video = document.querySelector('#video');
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
+
+let model;
+var streaming = false;
+
+function ControlStream(name) {
+    streaming = !streaming
+
+    if (streaming === true) {
+        console.log("CV for " + name + " activated");
+        handTrack.startVideo(video).then(status => {
+            if(status){
+                navigator.getUserMedia(
+                    { video:{} },
+                    stream => {
+                        video.srcObject = stream;
+                        trigger = setInterval(runDetection, 10, name);
+                    },
+                    err =>console.log(err)
+                );
+            }
+        });
     } else {
-        idiv.removeChild(document.getElementById("no-inports-label"));
-        inports.forEach(inport => {
-            const option = document.createElement("option");
-            option.innerText = inport.tag;
-            inportSelect.appendChild(option);
-        });
-        inportSelect.onchange = () => inportTag = inportSelect.value;
-        inportTag = inportSelect.value;
+        clearInterval(trigger);
+        handTrack.stopVideo(video)
+    }
+}
 
-        inportForm.onsubmit = (ev) => {
-            // Do this or else the page will reload
-            ev.preventDefault();
 
-            // Turn the text into a list of numbers (RNBO messages must be numbers, not text)
-            const values = inportText.value.split(/\s+/).map(s => parseFloat(s));
-            
-            // Send the message event to the RNBO device
-            let messageEvent = new RNBO.MessageEvent(RNBO.TimeNow, inportTag, values);
-            device.scheduleEvent(messageEvent);
+
+function runDetection(){
+    model.detect(video).then(predictions => {
+        model.renderPredictions(predictions, canvas, context, video);
+        if (predictions[0]) {
+            var xPrmtr = predictions[0].bbox[0] + (predictions[0].bbox[2] / 2);
+            var yPrmtr = predictions[0].bbox[1] + (predictions[0].bbox[3] / 2);
+            var zPrmtr = Math.max(predictions[0].bbox[2] / 640, predictions[0].bbox[3] / 480);
+            xPrmtr = xPrmtr / 640; // video width
+            yPrmtr = yPrmtr / 480; // video height
+            zPrmtr = Math.min(Math.max((zPrmtr - 0.2) * 2, 0), 1);
+
+            // Link sliders to computer vision
+            const sliderX = device.parametersById.get("carrier");
+            if (sliderX)
+                sliderX.value = xPrmtr * 127;
+            const sliderY = device.parametersById.get("modulator");
+            if (sliderY)
+                sliderY.value = yPrmtr * 127;
+            const sliderZ = device.parametersById.get("tremolo");
+            if (sliderZ)
+                sliderZ.value = zPrmtr * 127;
         }
-    }
-}
-
-function attachOutports(device) {
-    const outports = device.messages.filter(message => message.type === RNBO.MessagePortType.Outport);
-    if (outports.length < 1) {
-        document.getElementById("rnbo-console").removeChild(document.getElementById("rnbo-console-div"));
-        return;
-    }
-
-    document.getElementById("rnbo-console").removeChild(document.getElementById("no-outports-label"));
-    device.messageEvent.subscribe((ev) => {
-
-        // Message events have a tag as well as a payload
-        console.log(`${ev.tag}: ${ev.payload}`);
-
-        document.getElementById("rnbo-console-readout").innerText = `${ev.tag}: ${ev.payload}`;
     });
 }
-
-function loadPresets(device, patcher) {
-    let presets = patcher.presets || [];
-    if (presets.length < 1) {
-        document.getElementById("rnbo-presets").removeChild(document.getElementById("preset-select"));
-        return;
-    }
-
-    document.getElementById("rnbo-presets").removeChild(document.getElementById("no-presets-label"));
-    let presetSelect = document.getElementById("preset-select");
-    presets.forEach((preset, index) => {
-        const option = document.createElement("option");
-        option.innerText = preset.name;
-        option.value = index;
-        presetSelect.appendChild(option);
-    });
-    presetSelect.onchange = () => device.setPreset(presets[presetSelect.value].preset);
-}
-
-function makeMIDIKeyboard(device) {
-    let mdiv = document.getElementById("rnbo-clickable-keyboard");
-    if (device.numMIDIInputPorts === 0) return;
-
-    mdiv.removeChild(document.getElementById("no-midi-label"));
-
-    const midiNotes = [49, 52, 56, 63];
-    midiNotes.forEach(note => {
-        const key = document.createElement("div");
-        const label = document.createElement("p");
-        label.textContent = note;
-        key.appendChild(label);
-        key.addEventListener("pointerdown", () => {
-            let midiChannel = 0;
-
-            // Format a MIDI message paylaod, this constructs a MIDI on event
-            let noteOnMessage = [
-                144 + midiChannel, // Code for a note on: 10010000 & midi channel (0-15)
-                note, // MIDI Note
-                100 // MIDI Velocity
-            ];
-        
-            let noteOffMessage = [
-                128 + midiChannel, // Code for a note off: 10000000 & midi channel (0-15)
-                note, // MIDI Note
-                0 // MIDI Velocity
-            ];
-        
-            // Including rnbo.min.js (or the unminified rnbo.js) will add the RNBO object
-            // to the global namespace. This includes the TimeNow constant as well as
-            // the MIDIEvent constructor.
-            let midiPort = 0;
-            let noteDurationMs = 250;
-        
-            // When scheduling an event to occur in the future, use the current audio context time
-            // multiplied by 1000 (converting seconds to milliseconds) for now.
-            let noteOnEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000, midiPort, noteOnMessage);
-            let noteOffEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000 + noteDurationMs, midiPort, noteOffMessage);
-        
-            device.scheduleEvent(noteOnEvent);
-            device.scheduleEvent(noteOffEvent);
-
-            key.classList.add("clicked");
-        });
-
-        key.addEventListener("pointerup", () => key.classList.remove("clicked"));
-
-        mdiv.appendChild(key);
-    });
-}
+handTrack.load(modelParams).then(lmodel => {
+    model = lmodel;
+});
 
 setup();
